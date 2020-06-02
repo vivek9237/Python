8d8a47c7a6d0fa8847e72b188b2b71dd978d4f70
Vivek Kumar Mohanty <vivek.ku.mohanty@gmail.com>
2020-06-02 19:31:55 +0530

    Update ssh_using_python.py

--- python scripts for reference/python3/ssh_using_python.py
+++ python scripts for reference/python3/ssh_using_python.py
@@ -6,7 +6,7 @@ output_file = 'test.txt'
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('10.62.28.196', port=22119, username='root', password='Av3k5a')
+ssh.connect('xxx.xxx.xxx.xxx', port=22, username='root', password='root')
 
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('ls -l')
 output_1 = ssh_stdout.read()
